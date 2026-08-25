6924f15c748e51f30a63d79d872775589cf976e7
0x0nion <Dropup@protonmail.com>
2026-08-25 16:47:15 -0300

    23.08

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from database.connection import async_session, init_db
 
 from handlers import routers as all_routers
 from middlewares.db import DbSessionMiddleware
+from aiogram.fsm.storage.memory import MemoryStorage
 
 
 async def main():
@@ -35,4 +36,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
